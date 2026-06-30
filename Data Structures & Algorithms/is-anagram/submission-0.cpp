a9a9a5d45a7106e52f370dff2dee3375c509f1d7
Muskan Bidani <80620237+Muskan626bidani@users.noreply.github.com>
2026-07-01 00:41:09 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length())return false;
+        vector<int>v(26,0);
+        for(int i=0;i<s.length();i++){
+            v[s[i]-'a']++;
+
+        }
+        for(int i=0;i<t.length();i++){
+            if(v[t[i]-'a']>0)v[t[i]-'a']--;
+            else return false;
+        }
+        return true;
+    }
+};
